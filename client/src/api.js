fe38ba8d87dec071828fe8d5ca4fd5960b628411
Awais-niaiz <af514325@gmail.com>
2026-05-30 12:28:25 +0500

    Fix API URL with environment variables for local and production

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const login = async (username, password) => {
   const response = await axios.post(`${BASE_URL}/api/token/`, {
